d96657d2c9da4671752f676fd1f4845120d65b2a
RainBell98 <ttiger45@naver.com>
2023-11-23 19:09:13 +0900

    [Feat] 주문내역 삭제

---
+++ src/product/dto/review.credential.dto.ts
@@ -0,0 +1,7 @@
+export class ReviewCredentialDto{
+    user_id: number;
+    content: string;
+    rate: number;
+    goods_id:string;
+
+}
--- src/repository/review.repository.ts
+++ src/repository/review.repository.ts
@@ -1,9 +1,26 @@
 import { Review } from "src/entity/review.entity";
 import { DataSource, EntityRepository, Repository } from "typeorm";
+import {ReviewCredentialDto} from "../product/dto/review.credential.dto";
 
 @EntityRepository(Review)
 export class ReviewRepository extends Repository<Review>{
     constructor(private readonly dataSource:DataSource){
         super(Review, dataSource.createEntityManager());
     }
+
+    async writeReview(reviewCredentialDto: ReviewCredentialDto):Promise<boolean>{
+        const {user_id, goods_id, rate,content} = reviewCredentialDto;
+        const review = this.create({
+            user_id,goods_id,rate,content,
+            create_date: new Date(),
+        })
+        try {
+            await this.save(review);
+            return true;
+        }catch (e){
+            console.log(e)
+            return false;
+        }
+    }
+
 }
