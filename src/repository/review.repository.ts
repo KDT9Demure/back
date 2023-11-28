85441370c3f985e0ff88488ea0238c4e0acfb11d
Leewonno <dldnjssh123@naver.com>
2023-11-28 20:08:47 +0900

    [Fix] 리뷰 레파지토리 수정

--- src/repository/review.repository.ts
+++ src/repository/review.repository.ts
@@ -5,25 +5,8 @@ import {GoodsRepository} from "./goods.repository";
 
 @EntityRepository(Review)
 export class ReviewRepository extends Repository<Review>{
-    constructor(private readonly dataSource:DataSource,private goodsRepository:GoodsRepository){
+    constructor(private readonly dataSource:DataSource){
         super(Review, dataSource.createEntityManager());
-    }
-
-    async writeReview(reviewCredentialDto: ReviewCredentialDto):Promise<object>{
-        const {user_id, goods_id, rate,content} = reviewCredentialDto;
-        const review = this.create({
-            user_id,goods_id,rate,content,
-            create_date: new Date(),
-        })
-        try {
-            await this.save(review);
-            const goods = await this.goodsRepository.findOne({where:{id:goods_id}})
-            const rateUpdate = await this.goodsRepository.update({id:goods_id},{rate:goods.rate+rate})
-            return {result :true};
-        }catch (e){
-            console.log(e)
-            return { result: false};
-        }
     }
 
 }
