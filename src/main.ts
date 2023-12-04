d9b5e5c70e0a861a638bcbf5f6c2da357cbc2033
Leewonno <dldnjssh123@naver.com>
2023-12-04 20:44:56 +0900

    [Bugfix] cors 오류

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors();
   
   const config = new DocumentBuilder()
     .setTitle('Cats example')
